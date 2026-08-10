005180c0ad76c5d5be588067d53922d711aab9d6
Tavin Draughon <trdraughon@gmail.com>
2026-08-10 14:17:48 -0700

    Added player, tilemap system, collisions, and idle, runnning, and climbing animations.
    - Added a tile-map system (renderer and loader) with a rule tile that allows for a structure if the level
    - Added a animator and animation controller for the player, with an idle, run, and climb animation
    - Added physics based collisions with rigid body 2D and capsule collider for player, with tilemap and composite colliders for the tilemap system.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveInput;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
