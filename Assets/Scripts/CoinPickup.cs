8e52017eecfb25cde0f3f9d65c9b0bfac886da1d
Tavin Draughon <trdraughon@gmail.com>
2026-08-27 13:47:01 -0700

    Added Coin Pickup Sound Effect

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
 
+    [SerializeField] AudioClip coinPickupSFX;
     int coinCounter;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) {
             coinCounter++;
+            AudioSource.PlayClipAtPoint(coinPickupSFX, transform.position);
             Destroy(gameObject, 0.07f);
         }
     }
